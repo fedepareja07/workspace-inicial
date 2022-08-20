61c270e6c69ba5982b03ad0c7cf39b2126e2baaa
Santiago Pareja <federicopareja07@hotmail.com>
2022-08-20 18:06:46 -0300

    fede
    fede

---
+++ js/login.js
@@ -0,0 +1,28 @@
+let btn = document.getElementById("btn");
+let inputs = document.querySelectorAll(".control");
+let value = false;
+let mostrarAlerta = document.querySelector(".form-alert");
+let mostrarAlerta1 = document.querySelector(".form-alert1");
+
+mostrarAlerta.style.display="none";
+mostrarAlerta1.style.display="none";
+
+function buena(){
+for (let input of inputs){
+if(input.value.length !=0) {
+value = true;
+} else {
+value = false;
+}
+}
+return value;
+}
+
+btn.addEventListener("click", () => {
+if(buena()){
+    window.location.replace("index.html")
+} else {
+    mostrarAlerta.style.display="block";
+mostrarAlerta1.style.display="block";
+};
+});
--- js/products.js
+++ js/products.js
@@ -7,12 +7,40 @@ async function datos() {
       traerListado.innerHTML +=`
       <link href="css/styles.css" rel="stylesheet">
       <div class="coches">
-      <img src="${elemento.image}" class="autos">
-      <h3 class="titulo"> ${elemento.name}" </h3>
+      <img src="${elemento.image}" class="autos" >
+      <h4 class="titulo"> ${elemento.name} ${elemento.currency}  ${elemento.cost}" </h4>
       <p class="descripcion"> ${elemento.description}</p>
-      <p class="costo"> ${elemento.cost}</p>
+      <p class="vendidos"> ${elemento.soldCount} Vendidos</p>
+      
     </div>
       `
    }
 }
 datos();
+/*
+
+function mostrar(traidos.products){
+for (let elemento of traidos.products){
+   jsoN.innerHTML +=
+   `<div class="row">
+    <div class="list-group">
+     <div class="list-group-item list-group-item-action cursor-active">
+     <div class="row">
+       <div class="col-3">
+        <img class="img-thumbnail" src="${dat.image}">
+        </div>
+        <div class="col">
+        <div class="d-felx w100 justify-content-between">
+           <h4 class="text-muted"> ${dat.name, dat.currency,dat.cost}</h4>
+           <small class="text-muted">${dat.soldCount}</small>
+           </div>
+           <p class="mb-1">${dat.description}</p>
+           </div>
+           </div>
+           </div>
+           </div>
+           </div>
+`
+}
+}
+*/
